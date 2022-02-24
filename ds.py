013475ec41fbdc2e4482c3efc661c0f8a2d9f08d
Anna <@LAPTOP-3U01OB1R.(none)>
2022-02-24 09:56:43 +0600

    First commit

--- ds.py
+++ ds.py
@@ -4,20 +4,44 @@ from flask import request
 
 app = Flask(__name__)
 
+@app.route('/d')
+def show_site():
+    return render_template('hello.html')
+#http://127.0.0.1:5000/d
 
-@app.route('/')
+@app.route('/state')
 def show_post():
-    #render_template('hello.html')
-    a = request.form.get('https://ncov.blog/')
-    deaths = request.status.get('https://ncov.blog/')
-    return '<p>{deaths: %s}</p>' % deaths
+    #a = request.form.get('https://ncov.blog/')
+    deaths = request.headers.get('https://ncov.blog/')
+    return '<p>{    deaths:      %s}</p>' % deaths
+#http://127.0.0.1:5000/state
 
 
-@app.route('/query-example')
-def query_example():
-    # if key doesn't exist, returns None
-    language = request.args.get('language')
-    return '''<h1>The language value is: {}</h1>'''.format(language)
+@app.route('/state/<value>')
+def show_value(value):
+    if value=='deaths':
+        return  f'{escape(value)} :   393962903'
+    else:
+        return 'Invalid request'
+#http://127.0.0.1:5000/state/deaths
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    state = request.args.get('state')
+    error = None
+    if request.method == 'GET':
+        return '''
+                <h1>The deaths value is: {}</h1>'''.format(state)
+    else:
+        error = 'Invalid request'
+        return error
+    # следущий код выполняется при ошибке
+    # http://127.0.0.1:5000/?state=deaths
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('pages/404.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
